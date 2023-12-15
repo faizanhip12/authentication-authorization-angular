76b79297c251e88621a468fd9c91ffaaa7e676ec
faizanhip12 <113345962+faizanhip12@users.noreply.github.com>
2023-12-15 20:50:58 +0500

    changesSaved

--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -14,7 +15,11 @@ private baseUrl ="http://localhost:3000/api/v1/"
     this.isAuthenticated = !!localStorage.getItem(this.authSecretKey);
   }
   
-  login(username: string, password: string): any{
+  login(username: string, password: string): Observable<any>{
+
+      const user ={username,password}
+      return this.http.post<any>(`${this.baseUrl}/endpoint`, user );
+   
     // if (username === 'Jaydeep Patil' && password === 'Pass@123') {
     //   const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpheWRlZXAgUGF0aWwiLCJpYXQiOjE1MTYyMzkwMjJ9.yt3EOXf60R62Mef2oFpbFh2ihkP5qZ4fM8bjVnF8YhA'; // Generate or receive the token from your server
     //   localStorage.setItem(this.authSecretKey, authToken);
